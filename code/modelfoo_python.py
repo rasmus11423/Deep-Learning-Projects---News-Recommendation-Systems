8f98c7f7c3132e88387f845856d309a736b461cf
astridlh <75378190+astridlh@users.noreply.github.com>
2024-12-02 17:10:13 +0100

    fix: replace placeholder tokenizer

--- code/modelfoo_python.py
+++ code/modelfoo_python.py
@@ -8,14 +8,18 @@ from models.layers_pytorch import SelfAttention, AttLayer2
 from models.nrms_pytorch import NRMSModel, UserEncoder, NewsEncoder
 import numpy as np
 from pathlib import Path
+from utils._nlp import get_transformers_word_embeddings
 import polars as pl
 from utils._behaviors import create_binary_labels_column
 from utils._articles import create_article_id_to_value_mapping
+from transformers import AutoTokenizer, AutoModel
 from utils._constants import (
     DEFAULT_HISTORY_ARTICLE_ID_COL,
     DEFAULT_CLICKED_ARTICLES_COL,
     DEFAULT_INVIEW_ARTICLES_COL,
     DEFAULT_USER_COL,
+    DEFAULT_SUBTITLE_COL,
+    DEFAULT_TITLE_COL
 )
 
 # Training Function
@@ -111,8 +115,18 @@ if __name__ == "__main__":
         .pipe(create_binary_labels_column)
     )
 
+    # HuggingFace model configuration
+    TRANSFORMER_MODEL_NAME = "FacebookAI/xlm-roberta-base"
+    TEXT_COLUMNS_TO_USE = [DEFAULT_SUBTITLE_COL, DEFAULT_TITLE_COL]
+    MAX_TITLE_LENGTH = 30
+
+    # Load transformer model and tokenizer
+    transformer_model = AutoModel.from_pretrained(TRANSFORMER_MODEL_NAME)
+    transformer_tokenizer = AutoTokenizer.from_pretrained(TRANSFORMER_MODEL_NAME)
+
+    # Initialize word embeddings
+    word2vec_embedding = get_transformers_word_embeddings(transformer_model)
     # Create Article Mappings
-    word2vec_embedding = np.random.rand(5000, embedding_dim)  # Mock word2vec embeddings
     article_mapping = create_article_id_to_value_mapping(df=df_articles, value_col=TOKEN_COL)
 
     # Prepare Train and Validation Sets
