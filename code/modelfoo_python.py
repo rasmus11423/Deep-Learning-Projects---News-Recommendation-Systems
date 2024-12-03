bd2004f1266cb7426178d983b2af5fd20fc3b255
astridlh <75378190+astridlh@users.noreply.github.com>
2024-12-03 18:06:17 +0100

    fix: more prints st

--- code/modelfoo_python.py
+++ code/modelfoo_python.py
@@ -177,7 +177,7 @@ if __name__ == "__main__":
         batch_size=BATCH_SIZE,
     )
 
-    print("we now have data and tokenizer")
+    
 
     # Wrap in PyTorch DataLoader
     train_loader = DataLoader(train_dataloader, batch_size=None, shuffle=True)
@@ -202,6 +202,8 @@ if __name__ == "__main__":
         attention_hidden_dim=attention_hidden_dim,
     )
 
+    print("we now have data and tokenizer")
+
     # Initialize NRMS Model
     model = NRMSModel(
         user_encoder=user_encoder,
@@ -209,12 +211,16 @@ if __name__ == "__main__":
         embedding_dim=embedding_dim,
     )
 
+    print("model setup")
+
     # Training setup
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model.to(device)
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.Adam(model.parameters(), lr=0.001)
 
+    print("starting training")
+    
     # Training and Validation Loop
     epochs = 5
     for epoch in range(epochs):
